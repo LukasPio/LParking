9a66bd98fdf5c009d20a249cc05b6e5af3742ae0
LukasPio <lucaspio.galvao@gmail.com>
2025-01-15 19:55:31 -0300

    initial commit

---
+++ src/main/java/com/lucas/lparking/LParkingApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.lparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LParkingApplication.class, args);
+    }
+
+}
